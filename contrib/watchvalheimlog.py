910d3ee6296e0ec56c5a2091377392f5de2b4948
Bob Harold <bgithub@halfgoat.com>
2022-10-02 17:04:41 -0400

    add input checks

--- contrib/watchvalheimlog.py
+++ contrib/watchvalheimlog.py
@@ -39,12 +39,32 @@ def shutdown():
     result=subprocess.run(shutdownhook)
     print("shutdown result:",result)    # probably never see this
 
+def usage(msg=None):
+    '''print usage and/or error'''
+    print("Usage: watchvalehimlog.py <logfile>")
+    if msg:
+        print(msg)
+        os.exit(1)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("logfile", help="logfile to tail")
     parser.add_argument("--debug", "-d")
     args = parser.parse_args()
+    # check args
+    backuphook=os.environ.get('POST_BACKUP_HOOK')
+    if not backuphook:
+        usage("missing env var POST_BACKUP_HOOK")
+    shutdownhook=os.environ.get('SHUTDOWN_HOOK')
+    if not shutdownhook:
+        usage("missing env var SHUTDOWN_HOOK")
     logfile = args.logfile
+    # test logfile
+    try:
+        with open(logfile,"rt") as fh:
+            fh.seek(0,2)    # seek end of file
+    except FileNotFoundError as e:
+        usage("file not found or not readable/seekable " + logfile + e)
     debug = args.debug
     if debug:
         print("debug enabled",flush=True)
