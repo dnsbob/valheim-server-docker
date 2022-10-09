c7a4795b8d77639357b80b4299d793d0412cb74e
Bob Harold <bgithub@halfgoat.com>
2022-10-08 20:25:37 -0400

    add valheim-backup-hanoi-aws.sh

---
+++ contrib/valheim-backup-hanoi-aws.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# valheim-backup-hanoi-aws.sh  WORLD_NAME  WORLD_PATH  max basedir  BACKUP_BUCKET  BACKUP_PATH
+
+if [ $# -ne 6 ]; then
+  echo “usage:  $0 WORLD_NAME  WORLD_PATH  max basedir  BACKUP_BUCKET  BACKUP_PATH”
+  exit 1
+fi
+
+WORLD_NAME =$1
+WORLD_PATH=$2
+max=$3
+basedir=$4
+BACKUP_BUCKET=$5
+BACKUP_PATH=$6
+
+zipfile=`valheim-save-zip.sh echo $WORLD_NAME $WORLD_PATH`
+hanoi-backup.sh $max $basedir $zipfile
+aws_s3_backup_sync.sh $basedir $BACKUP_BUCKET $BACKUP_PATH
