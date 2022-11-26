5f4b1acd981df1e7e5912c6e5a4385d564bac94f
dnsbob <30326095+dnsbob@users.noreply.github.com>
2022-11-25 20:01:39 -0500

    Update valheim-backup-hanoi-aws.sh
    add a log line

--- contrib/valheim-backup-hanoi-aws.sh
+++ contrib/valheim-backup-hanoi-aws.sh
@@ -13,6 +13,8 @@ basedir=$4
 BACKUP_BUCKET=$5
 BACKUP_PATH=$6
 
+echo "world=${WORLD_PATH}/${WORLD_NAME} max=$max basedir=$basedir bucket=${BACKUP_BUCKET} path=$BACKUP_PATH" >> /tmp/$0.log
+
 zipfile=`bin/valheim-save-zip.sh echo $WORLD_NAME $WORLD_PATH | tail -1`
 bin/hanoi-backup.sh $max $basedir $zipfile
 bin/aws_s3_backup_sync.sh $basedir $BACKUP_BUCKET $BACKUP_PATH
