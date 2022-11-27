44ef3390c86b217bd582db665ba607e575858b51
Bob Harold <bgithub@halfgoat.com>
2022-11-26 20:42:57 -0500

    add logging

--- contrib/start-watch.sh
+++ contrib/start-watch.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# start-watch.sh
+
+log=/tmp/$0.log
+echo "`date` $0 started" >> $log
+
 export BACKUP_BUCKET=dnsbob-valheim
 export BACKUP_PATH=backup
 export WORLD_NAME=dnsbob20220218
@@ -5,4 +11,4 @@ export WORLD_PATH=/root/valheim-server/config/worlds_local
 export HANOI_BASE=/root/valheim-server/hanoi
 export BACKUP_SCRIPT=/home/ec2-user/valheim-server-docker/contrib/valheim-backup-hanoi-aws.sh
 export WATCH_SAVE_HOOK="$BACKUP_SCRIPT $WORLD_NAME $WORLD_PATH 6 $HANOI_BASE $BACKUP_BUCKET $BACKUP_PATH"
-/home/ec2-user/valheim-server-docker/contrib/watchvalheimlog.py /root/valheim-server/log/supervisor/valheim-server-stdout---supervisor-*.log
+/home/ec2-user/valheim-server-docker/contrib/watchvalheimlog.py /root/valheim-server/log/supervisor/valheim-server-stdout---supervisor-*.log 2>&1 >> $log
